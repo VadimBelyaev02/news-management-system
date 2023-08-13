c3fb7223a0aab22c36660f895a0409d9c0166aea
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-08-13 13:44:08 +0300

    Created CRUD for entity user

--- user-service/src/main/java/com/vadim/userservice/model/entity/User.java
+++ user-service/src/main/java/com/vadim/userservice/model/entity/User.java
@@ -1,12 +1,32 @@
 package com.vadim.userservice.model.entity;
 
+import com.vadim.userservice.model.enums.UserRole;
+import com.vadim.userservice.model.enums.UserStatus;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String username;
+    private String password;
+    private String email;
+
+    @CreatedDate
+    @Column(name = "created_date")
+    private String createdDate;
+    private UserStatus status;
+    private UserRole role;
 }
